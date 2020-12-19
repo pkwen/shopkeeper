1fb58bfa2e3d7f49b661305eb8219e6280d0ddcb
subclinical <kaiwen006@gmail.com>
2020-12-19 14:24:22 -0500

    product endpoints

--- app.py
+++ app.py
@@ -1,17 +1,27 @@
 import os
-from flask import Flask, escape, request, render_template, abort
+from flask import Flask, escape, request, render_template, abort, jsonify, make_response, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
 from flask_migrate import Migrate
 from datetime import datetime
+from flask_bcrypt import Bcrypt
+import pandas as pd
+import csv
+import codecs
 import graphene
-print(os.environ['APP_SETTINGS'])
+import flask_login
+
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+bcrypt = Bcrypt(app)
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
+engine = create_engine(os.environ['DATABASE_URL'])
 
-from models import Business
+import models
 class Query(graphene.ObjectType):
     hello = graphene.String(description='A typical hello world')
 
@@ -43,6 +53,63 @@ def hello():
 @app.route("/hidden/<int:index>")
 def secret(index):
     return render_template("dynamic.html", index=index)
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.form:
+        user = models.Admin.query.filter_by(email=request.form['email']).first()
+        if user is None or not user.validate(request.form['password']):
+            return make_response({"message": "wrong credentials"})
+        flask_login.login_user(user, remember=True)
+        # return redirect(url_for('login'))
+        return make_response(jsonify(user.serialize()), 200)
+
+@app.route("/setpw", methods=["POST"])
+def setpw():
+    if request.form:
+        user = models.Admin.query.first()
+        user.password = request.form['password']
+        db.session.commit()
+    return make_response(jsonify(user.serialize()), 200)
+
+@login_manager.user_loader
+def user_loader(user_id):
+    """Given *user_id*, return the associated User object.
+
+    :param unicode user_id: user_id (email) user to retrieve
+
+    """
+    return models.Admin.query.get(user_id)
+
+@app.route("/products/upload_csv", methods=["POST"])
+def upload_csv():
+    print(flask_login.current_user.businesses)
+    flask_file = request.files['file']
+    if not flask_file:
+        return 'Upload a CSV file'
+    data = pd.read_csv(request.files['file'])
+    data.to_sql("products", engine, if_exists="append", index=False)
+    return data.to_json()
+    # data = []
+    # stream = codecs.iterdecode(flask_file.stream, 'utf-8')
+    # for row in csv.reader(stream, dialect=csv.excel):
+    #     if row:
+    #         data.append(row)
+            
+    # return jsonify(data)
+@app.route("/products", methods=["GET", "POST"])
+def products():
+    if request.method == "POST":
+        product = models.Product(
+            name="One",
+            quantity=500,
+            price=12.99,
+            description="yes"
+        )
+        db.session.add(product)
+        db.session.commit()
+        return make_response(jsonify(product.serialize()), 200)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- models.py
+++ models.py
@@ -1,21 +1,67 @@
-from app import db
-from sqlalchemy.dialects.postgresql import JSON
+from app import db, bcrypt, login_manager
+from sqlalchemy.dialects.postgresql import JSON, UUID
+from sqlalchemy.ext.hybrid import hybrid_property, hybrid_method
+from flask_login import UserMixin
+import enum
 
-class AdminBusiness(db.Model):
-    __tablename__ = 'admin_business'
-    admin_id = db.Column(db.Integer, db.ForeignKey('admins.id'), primary_key=True)
-    business_id = db.Column(db.Integer, db.ForeignKey('businesses.id'), primary_key=True)
-    role = db.Column(db.String())
-    admin = db.relationship("Admin", back_populates="businesses")
-    business = db.relationship("Business", back_populates="admins")
+import uuid
+
+    
+class Admin(UserMixin, db.Model):
+    __tablename__ = 'admins'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    uuid = db.Column(UUID(as_uuid=True), default=uuid.uuid4, unique=True, nullable=False)
+    first_name = db.Column(db.String())
+    last_name = db.Column(db.String())
+    email = db.Column(db.String(), unique=True)
+    _password = db.Column(db.LargeBinary())
+    mobile = db.Column(db.String(), unique=True)
+    businesses = db.relationship("AdminBusiness", back_populates="admin")
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
+    
+    def serialize(self):
+        return {
+            'email': self.email
+        }
+    
+    @hybrid_property
+    def password(self):
+        return self._password
+    
+    @password.setter
+    def password(self, plaintext):
+        self._password = bcrypt.generate_password_hash(plaintext)
+        
+    @hybrid_method
+    def validate(self, plaintext_password):
+        return bcrypt.check_password_hash(self.password, plaintext_password)
+        
+@login_manager.user_loader
+def user_loader(user_id):
+    """Given *user_id*, return the associated User object.
+
+    :param unicode user_id: user_id (email) user to retrieve
+
+    """
+    return Admin.query.get(user_id)
+
 class Business(db.Model):
     __tablename__ = 'businesses'
 
     id = db.Column(db.Integer, primary_key=True)
+    uuid = db.Column(UUID(as_uuid=True), default=uuid.uuid4, unique=True, nullable=False)
     owner = db.Column(db.String())
     industry = db.Column(db.String())
     name = db.Column(db.String())
     admins = db.relationship("AdminBusiness", back_populates="business")
+    branches = db.relationship("Branch", back_populates="business")
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
 
     def __init__(self, owner, industry, name):
         self.owner = owner
@@ -24,22 +70,116 @@ class Business(db.Model):
 
     def __repr__(self):
         return '<id {}>'.format(self.id)
-    
-class Admin(db.Model):
-    __tablename__ = 'admins'
-    
-    id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String())
-    last_name = db.Column(db.String())
-    email = db.Column(db.String())
-    mobile = db.Column(db.String())
-    businesses = db.relationship("AdminBusiness", back_populates="admin")
-    
-    def __init__(self, email, first_name, last_name, mobile):
-        self.email=email
-        self.first_name=first_name
-        self.last_name=last_name
-        self.mobile=mobile
 
-    def __repr__(self):
-        return '<id {}>'.format(self.id)
+
+class AdminBusiness(db.Model):
+    __tablename__ = 'admin_business'
+    admin_id = db.Column(db.Integer, db.ForeignKey('admins.id'), primary_key=True)
+    business_id = db.Column(db.Integer, db.ForeignKey('businesses.id'), primary_key=True)
+    role = db.Column(db.String())
+    admin = db.relationship("Admin", back_populates="businesses")
+    business = db.relationship("Business", back_populates="admins")
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
+    
+class Branch(db.Model):
+    __tablename__ = 'branches'
+    id = db.Column(db.Integer, primary_key=True)
+    uuid = db.Column(UUID(as_uuid=True), default=uuid.uuid4, unique=True, nullable=False)
+    name = db.Column(db.String())
+    location = db.Column(db.String())
+    business_id = db.Column(db.Integer, db.ForeignKey('businesses.id'))
+    business = db.relationship("Business", back_populates="branches")
+    stations = db.relationship("Station", back_populates="branch")
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
+    
+class FoodEnum(enum.Enum):
+    starter = 0
+    
+    
+class Product(db.Model):
+    __tablename__ = 'products'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    stock = db.Column(db.Integer)
+    price = db.Column(db.Float)
+    category = db.Column(db.String)
+    description = db.Column(db.Text)
+    branch_id = db.Column(db.Integer, db.ForeignKey('branches.id'))
+    components = db.relationship("ProductComponent", back_populates="product")
+    orders = db.relationship("OrderProduct", back_populates="product")
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'stock': self.stock,
+            'price': self.price,
+            'description': self.description,
+            'branch': self.branch_id
+        }
+    
+class ProductComponent(db.Model):
+    __tablename__ = 'products_components'
+    id = db.Column(db.Integer, primary_key=True)
+    product_id = db.Column(db.Integer, db.ForeignKey('products.id'))
+    component_id = db.Column(db.Integer, db.ForeignKey('components.id'))
+    quantity = db.Column(db.Integer)
+    product = db.relationship("Product", back_populates="components")
+    component = db.relationship("Component", back_populates="products")
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
+    
+class Component(db.Model):
+    __tablename__ = 'components'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    unit = db.Column(db.String)
+    stock = db.Column(db.Integer)
+    cost = db.Column(db.Float)
+    supplier = db.Column(db.String)
+    branch_id = db.Column(db.Integer, db.ForeignKey('branches.id'))
+    products = db.relationship("ProductComponent", back_populates="component")
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
+    
+class Order(db.Model):
+    __tablename__ = 'orders'
+    id = db.Column(db.Integer, primary_key=True)
+    uuid = db.Column(UUID(as_uuid=True), default=uuid.uuid4, unique=True, nullable=False)
+    total = db.Column(db.Float)
+    supplier = db.Column(db.String)
+    branch_id = db.Column(db.Integer, db.ForeignKey('branches.id'))
+    products = db.relationship("OrderProduct", back_populates="order")
+    station_id = db.Column(db.Integer, db.ForeignKey('stations.id'))
+    station = db.relationship("Station", back_populates="orders")
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
+    
+class OrderProduct(db.Model):
+    __tablename__ = 'orders_products'
+    id = db.Column(db.Integer, primary_key=True)
+    order_id = db.Column(db.Integer, db.ForeignKey('orders.id'))
+    product_id = db.Column(db.Integer, db.ForeignKey('products.id'))
+    order = db.relationship("Order", back_populates="products")
+    product = db.relationship("Product", back_populates="orders")
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
+    
+class Station(db.Model):
+    __tablename__ = 'stations'
+    id = db.Column(db.Integer, primary_key=True)
+    codename = db.Column(db.String)
+    capacity = db.Column(db.Integer)
+    qr_code_img = db.Column(db.String)
+    uuid = db.Column(UUID(as_uuid=True), default=uuid.uuid4, unique=True, nullable=False)
+    qr_code_content = db.Column(db.String)
+    branch_id = db.Column(db.Integer, db.ForeignKey('branches.id'))
+    branch = db.relationship("Branch", back_populates="stations")
+    orders = db.relationship("Order", back_populates="station")
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
+    

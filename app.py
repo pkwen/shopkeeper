549491503db4ad41e49de1cbda371941cae75957
subclinical <kaiwen006@gmail.com>
2020-12-22 16:06:24 -0500

    API WIP

--- app.py
+++ app.py
@@ -7,26 +7,27 @@ from datetime import datetime
 from flask_bcrypt import Bcrypt
 import pandas as pd
 import csv
+import uuid
 import codecs
 import graphene
 import flask_login
 
 app = Flask(__name__)
-app.config.from_object(os.environ['APP_SETTINGS'])
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config.from_object(os.environ["APP_SETTINGS"])
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 bcrypt = Bcrypt(app)
 login_manager = flask_login.LoginManager()
 login_manager.init_app(app)
-engine = create_engine(os.environ['DATABASE_URL'])
+engine = create_engine(os.environ["DATABASE_URL"])
 
 import models
 class Query(graphene.ObjectType):
-    hello = graphene.String(description='A typical hello world')
+    hello = graphene.String(description="A typical hello world")
 
     def resolve_hello(self, info):
-        return 'World'
+        return "World"
 
 schema = graphene.Schema(query=Query)
 
@@ -39,7 +40,7 @@ schema = graphene.Schema(query=Query)
 #     def __repr__(self):
 #         return "<Task %r>" % self.id
 
-@app.route('/')
+@app.route("/")
 def hello():
     query = """
     query SayHello {
@@ -57,18 +58,18 @@ def secret(index):
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.form:
-        user = models.Admin.query.filter_by(email=request.form['email']).first()
-        if user is None or not user.validate(request.form['password']):
+        user = models.Admin.query.filter_by(email=request.form["email"]).first()
+        if user is None or not user.validate(request.form["password"]):
             return make_response({"message": "wrong credentials"})
         flask_login.login_user(user, remember=True)
-        # return redirect(url_for('login'))
+        # return redirect(url_for("login"))
         return make_response(jsonify(user.serialize()), 200)
 
 @app.route("/setpw", methods=["POST"])
 def setpw():
     if request.form:
         user = models.Admin.query.first()
-        user.password = request.form['password']
+        user.password = request.form["password"]
         db.session.commit()
     return make_response(jsonify(user.serialize()), 200)
 
@@ -81,25 +82,18 @@ def user_loader(user_id):
     """
     return models.Admin.query.get(user_id)
 
-@app.route("/products/upload_csv", methods=["POST"])
-def upload_csv():
-    print(flask_login.current_user.businesses)
-    flask_file = request.files['file']
-    if not flask_file:
-        return 'Upload a CSV file'
-    data = pd.read_csv(request.files['file'])
-    data.to_sql("products", engine, if_exists="append", index=False)
-    return data.to_json()
-    # data = []
-    # stream = codecs.iterdecode(flask_file.stream, 'utf-8')
-    # for row in csv.reader(stream, dialect=csv.excel):
-    #     if row:
-    #         data.append(row)
-            
-    # return jsonify(data)
+
+#product
 @app.route("/products", methods=["GET", "POST"])
 def products():
-    if request.method == "POST":
+    if request.method == "GET":
+        branch = models.Branch.query.filter_by(uuid = request.args.get("branch")).first()
+        if branch:
+            return make_response(jsonify(list(map(lambda product: product.serialize(), branch.products))), 200)
+        else:
+            return "Branch not found"
+    elif request.method == "POST":
+        # validate form
         product = models.Product(
             name="One",
             quantity=500,
@@ -110,6 +104,60 @@ def products():
         db.session.commit()
         return make_response(jsonify(product.serialize()), 200)
 
+@app.route("/products/upload_csv", methods=["POST"])
+def upload_csv():
+    print(flask_login.current_user.businesses)
+    flask_file = request.files["file"]
+    branch = request.form["branch"]
+    if not flask_file:
+        return "Upload a CSV file"
+    if not branch:
+        return "Branch missing"
+    data = pd.read_csv(request.files["file"])
+    data.insert(2, "branch_id", branch)
+    data["uuid"] = [str(uuid.uuid4()) for _ in data["name"]]
+    data["description"] = data["description"].apply(lambda desc:[desc])
+    data.to_sql("products", engine, if_exists="append", index=False)
+    return data.to_json()
+
+    
+#station
+@app.route("/stations", methods=["GET", "POST"])
+def stations():
+    if request.method == "GET":
+        if not request.args.get("branch"):
+            return "Branch missing"
+        branch = models.Branch.query.filter_by(uuid = request.args.get("branch")).first()
+        return make_response(jsonify(list(map(lambda station: station.serialize(), branch.stations))))
+    elif request.method == "POST":
+        stn = models.Station(codename="table_4", capacity=2, branch_id=1)
+        db.session.add(stn)
+        db.session.commit()
+        return stn.serialize()
+
+@app.route("/station/<uuid:int>")
+def get_station():
+    return models.Station.query.filter_by(uuid = uuid).first()
+
+#order
+@app.route("/orders", methods=["GET", "POST"])
+def orders():
+    if request.method == "GET":
+        if not request.args.get("branch"):
+            return "Branch missing"
+        branch = models.Branch.query.filter_by(uuid = request.args.get("branch")).first()
+        return make_response(jsonify(list(map(lambda order: order.serialize(), branch.orders))))
+    elif request.method == "POST":
+        if not request.form["products"] or not request.form["station"]:
+            return "Parameter missing"
+        order = models.Order(total=110, station_id=7, branch_id=1)
+        db.session.add(order)
+        db.session.commit()
+        return order.serialize()
+
+@app.route("/order/<uuid:int>")
+def get_order():
+    return models.Station.query.filter_by(uuid = uuid).first()
 
 if __name__ == "__main__":
     app.run(debug=True)
--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from app import db, bcrypt, login_manager
+from app import db, bcrypt, login_manager, jsonify
 from sqlalchemy.dialects.postgresql import JSON, UUID
 from sqlalchemy.ext.hybrid import hybrid_property, hybrid_method
 from flask_login import UserMixin
@@ -8,7 +8,7 @@ import uuid
 
     
 class Admin(UserMixin, db.Model):
-    __tablename__ = 'admins'
+    __tablename__ = "admins"
     
     id = db.Column(db.Integer, primary_key=True)
     uuid = db.Column(UUID(as_uuid=True), default=uuid.uuid4, unique=True, nullable=False)
@@ -22,11 +22,11 @@ class Admin(UserMixin, db.Model):
     updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
 
     def __repr__(self):
-        return '<id {}>'.format(self.id)
+        return "<id {}>".format(self.id)
     
     def serialize(self):
         return {
-            'email': self.email
+            "email": self.email
         }
     
     @hybrid_property
@@ -51,7 +51,7 @@ def user_loader(user_id):
     return Admin.query.get(user_id)
 
 class Business(db.Model):
-    __tablename__ = 'businesses'
+    __tablename__ = "businesses"
 
     id = db.Column(db.Integer, primary_key=True)
     uuid = db.Column(UUID(as_uuid=True), default=uuid.uuid4, unique=True, nullable=False)
@@ -69,13 +69,13 @@ class Business(db.Model):
         self.name = name
 
     def __repr__(self):
-        return '<id {}>'.format(self.id)
+        return "<id {}>".format(self.id)
 
 
 class AdminBusiness(db.Model):
-    __tablename__ = 'admin_business'
-    admin_id = db.Column(db.Integer, db.ForeignKey('admins.id'), primary_key=True)
-    business_id = db.Column(db.Integer, db.ForeignKey('businesses.id'), primary_key=True)
+    __tablename__ = "admin_business"
+    admin_id = db.Column(db.Integer, db.ForeignKey("admins.id"), primary_key=True)
+    business_id = db.Column(db.Integer, db.ForeignKey("businesses.id"), primary_key=True)
     role = db.Column(db.String())
     admin = db.relationship("Admin", back_populates="businesses")
     business = db.relationship("Business", back_populates="admins")
@@ -83,14 +83,15 @@ class AdminBusiness(db.Model):
     updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
     
 class Branch(db.Model):
-    __tablename__ = 'branches'
+    __tablename__ = "branches"
     id = db.Column(db.Integer, primary_key=True)
     uuid = db.Column(UUID(as_uuid=True), default=uuid.uuid4, unique=True, nullable=False)
     name = db.Column(db.String())
     location = db.Column(db.String())
-    business_id = db.Column(db.Integer, db.ForeignKey('businesses.id'))
+    business_id = db.Column(db.Integer, db.ForeignKey("businesses.id"))
     business = db.relationship("Business", back_populates="branches")
     stations = db.relationship("Station", back_populates="branch")
+    products = db.relationship("Product")
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
     
@@ -99,14 +100,16 @@ class FoodEnum(enum.Enum):
     
     
 class Product(db.Model):
-    __tablename__ = 'products'
+    __tablename__ = "products"
     id = db.Column(db.Integer, primary_key=True)
+    uuid = db.Column(UUID(as_uuid=True), default=uuid.uuid4, unique=True, nullable=False)
     name = db.Column(db.String)
     stock = db.Column(db.Integer)
     price = db.Column(db.Float)
     category = db.Column(db.String)
-    description = db.Column(db.Text)
-    branch_id = db.Column(db.Integer, db.ForeignKey('branches.id'))
+    description = db.Column(db.ARRAY(db.Text), default=[])
+    branch_id = db.Column(db.Integer, db.ForeignKey("branches.id"))
+    branch = db.relationship("Branch")
     components = db.relationship("ProductComponent", back_populates="product")
     orders = db.relationship("OrderProduct", back_populates="product")
     created_at = db.Column(db.DateTime, server_default=db.func.now())
@@ -114,19 +117,19 @@ class Product(db.Model):
 
     def serialize(self):
         return {
-            'id': self.id,
-            'name': self.name,
-            'stock': self.stock,
-            'price': self.price,
-            'description': self.description,
-            'branch': self.branch_id
+            "id": self.id,
+            "name": self.name,
+            "stock": self.stock,
+            "price": self.price,
+            "description": self.description,
+            "branch": self.branch_id
         }
     
 class ProductComponent(db.Model):
-    __tablename__ = 'products_components'
+    __tablename__ = "products_components"
     id = db.Column(db.Integer, primary_key=True)
-    product_id = db.Column(db.Integer, db.ForeignKey('products.id'))
-    component_id = db.Column(db.Integer, db.ForeignKey('components.id'))
+    product_id = db.Column(db.Integer, db.ForeignKey("products.id"))
+    component_id = db.Column(db.Integer, db.ForeignKey("components.id"))
     quantity = db.Column(db.Integer)
     product = db.relationship("Product", back_populates="components")
     component = db.relationship("Component", back_populates="products")
@@ -134,52 +137,68 @@ class ProductComponent(db.Model):
     updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
     
 class Component(db.Model):
-    __tablename__ = 'components'
+    __tablename__ = "components"
     id = db.Column(db.Integer, primary_key=True)
+    uuid = db.Column(UUID(as_uuid=True), default=uuid.uuid4, unique=True, nullable=False)
     name = db.Column(db.String)
     unit = db.Column(db.String)
     stock = db.Column(db.Integer)
     cost = db.Column(db.Float)
     supplier = db.Column(db.String)
-    branch_id = db.Column(db.Integer, db.ForeignKey('branches.id'))
+    branch_id = db.Column(db.Integer, db.ForeignKey("branches.id"))
     products = db.relationship("ProductComponent", back_populates="component")
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
     
 class Order(db.Model):
-    __tablename__ = 'orders'
+    __tablename__ = "orders"
     id = db.Column(db.Integer, primary_key=True)
     uuid = db.Column(UUID(as_uuid=True), default=uuid.uuid4, unique=True, nullable=False)
     total = db.Column(db.Float)
-    supplier = db.Column(db.String)
-    branch_id = db.Column(db.Integer, db.ForeignKey('branches.id'))
+    status = db.Column(db.String, default="requested")
+    branch_id = db.Column(db.Integer, db.ForeignKey("branches.id"))
     products = db.relationship("OrderProduct", back_populates="order")
-    station_id = db.Column(db.Integer, db.ForeignKey('stations.id'))
+    station_id = db.Column(db.Integer, db.ForeignKey("stations.id"))
     station = db.relationship("Station", back_populates="orders")
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
     
+    def serialize(self):
+        return {
+            "uuid": self.uuid,
+            "total": self.total,
+            "station": self.station.codename,
+            "products": self.products
+        }
+    
 class OrderProduct(db.Model):
-    __tablename__ = 'orders_products'
+    __tablename__ = "orders_products"
     id = db.Column(db.Integer, primary_key=True)
-    order_id = db.Column(db.Integer, db.ForeignKey('orders.id'))
-    product_id = db.Column(db.Integer, db.ForeignKey('products.id'))
+    order_id = db.Column(db.Integer, db.ForeignKey("orders.id"))
+    product_id = db.Column(db.Integer, db.ForeignKey("products.id"))
     order = db.relationship("Order", back_populates="products")
     product = db.relationship("Product", back_populates="orders")
+    quantity = db.Column(db.Integer)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
     
 class Station(db.Model):
-    __tablename__ = 'stations'
+    __tablename__ = "stations"
     id = db.Column(db.Integer, primary_key=True)
     codename = db.Column(db.String)
     capacity = db.Column(db.Integer)
     qr_code_img = db.Column(db.String)
     uuid = db.Column(UUID(as_uuid=True), default=uuid.uuid4, unique=True, nullable=False)
     qr_code_content = db.Column(db.String)
-    branch_id = db.Column(db.Integer, db.ForeignKey('branches.id'))
+    branch_id = db.Column(db.Integer, db.ForeignKey("branches.id"))
     branch = db.relationship("Branch", back_populates="stations")
     orders = db.relationship("Order", back_populates="station")
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
+    
+    def serialize(self):
+        return {
+            "codename": self.codename,
+            "capacity": self.capacity
+        }
     
